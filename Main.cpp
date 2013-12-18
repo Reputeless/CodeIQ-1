1d02c6b6db39e13ac1972e94edc44f60ee77ae30
Reputeless <reputeless@gmail.com>
2013-12-18 10:42:38 +0900

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -3,19 +3,19 @@
 
 void Main()
 {
-	const Polygon polygon(CreateRect(10, 40, 100, 200));
+	const Polygon polygon(codeIQ::CreateRect(10, 40, 100, 200));
 
-	//const Polygon polygon(CreatePlus(100, 60, { 20, 20 }));
+	//const Polygon polygon(codeIQ::CreatePlus(100, 60, { 20, 20 }));
 
-	//const Polygon polygon(CreatePentagon(120,Radians(25)));
+	//const Polygon polygon(codeIQ::CreatePentagon(120,Radians(25)));
 
-	//const Polygon polygon(CreateHexagon(120));
+	//const Polygon polygon(codeIQ::CreateHexagon(120));
 
-	//const Polygon polygon(CreateNgon(8,120));
+	//const Polygon polygon(codeIQ::CreateNgon(8,120));
 
-	//const Polygon polygon(CreateStar(120.0));
+	//const Polygon polygon(codeIQ::CreateStar(120.0));
 
-	//const Polygon polygon(CreateNStar(32,120.0,80.0));
+	//const Polygon polygon(codeIQ::CreateNStar(32,120.0,80.0));
 
 	while (System::Update())
 	{
