0282e3ef7f1ec5ce653985f6aa8db0559b8e72f0
Reputeless <reputeless@gmail.com>
2013-12-20 09:59:39 +0900

    Update Shapes.cpp

--- Shapes.cpp
+++ Shapes.cpp
@@ -9,31 +9,31 @@ namespace codeIQ
 
 	std::vector<Vec2> CreatePlus(double r, double width, const Vec2& center)
 	{
-		return{};
+
 	}
 
 	std::vector<Vec2> CreatePentagon(double r, double angle, const Vec2& center)
 	{
-		return{};
+
 	}
 
 	std::vector<Vec2> CreateHexagon(double r, double angle, const Vec2& center)
 	{
-		return{};
+
 	}
 
 	std::vector<Vec2> CreateNgon(int n, double r, double angle, const Vec2& center)
 	{
-		return{};
+
 	}
 
 	std::vector<Vec2> CreateStar(double r, double angle, const Vec2& center)
 	{
-		return{};
+
 	}
 
 	std::vector<Vec2> CreateNStar(int n, double rOuter, double rInner, double angle, const Vec2& center)
 	{
-		return{};
+
 	}
 }
